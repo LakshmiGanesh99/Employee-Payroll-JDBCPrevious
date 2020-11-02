d6cf01f932adec6cc182c147daf9c9e61ddf150b
LakshmiGanesh <71899781+LakshmiGanesh99@users.noreply.github.com>
2020-11-02 20:49:21 +0530

    [LakshmiGanesh] Create: database connection created

--- EmpPayrollJDBC/src/main/java/com/capg/emppayrolljdbc/PayrollService.java
+++ EmpPayrollJDBC/src/main/java/com/capg/emppayrolljdbc/PayrollService.java
@@ -1,13 +1,25 @@
 package com.capg.emppayrolljdbc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.sql.*;
+import java.util.Enumeration;
+
+public class PayrollService {
+
+	public static final String URL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+	public static final String USER = "root";
+	public static final String PASSWORD = "Lakshmi@99";
+
+	public static void main(String[] args) {
+		Connection connection;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Driver loaded!");
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+			System.out.println(connection + " Connection established!");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("Cannot find the driver in the classpath", e);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
